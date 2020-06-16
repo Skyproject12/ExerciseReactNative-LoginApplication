eec41c01742d7ed2d17659fb4bc2f94c95e3abee
Kadek Sumendra <sumendrakadek108@gmail.com>
2020-06-16 21:30:49 +0700

    make display for login

--- app/component/Home/index.js
+++ app/component/Home/index.js
@@ -1,20 +1,44 @@
 import React, {Component} from 'react';
 import {Text, View, TextInput, Button} from 'react-native';
+import styles from './style';
 
 class HomeScreen extends Component {
   goToDashboardScreen() {
     this.props.navigation.navigate('Dashboard');
   }
+
+  state = {
+    username: '',
+    password: '',
+  };
+
+  checkLogin() {
+    const {username, password} = this.state;
+    if (username === 'admin' && password === 'admin') {
+      console.log('masuk');
+    }
+    console.log('hasilnya', username, password);
+  }
   render() {
+    const {heading, input, viewButton, description} = styles;
     return (
-      <View>
-        <Text>Login into the application</Text>
-        <TextInput placeholder="enter your name" />
-        <TextInput placeholder="enter your password" />
-        <Button
-          title="Login"
-          onPress={() => this.props.navigation.navigate('Dashboard')}
+      <View style>
+        <Text style={heading}>Login</Text>
+        <Text style={description}>Login into the application</Text>
+        <TextInput
+          onChangeText={(text) => this.setState({username: text})}
+          style={input}
+          placeholder="enter your name"
         />
+        <TextInput
+          onChangeText={(text) => this.setState({password: text})}
+          secureTextEntry={true}
+          style={input}
+          placeholder="enter your password"
+        />
+        <View style={viewButton}>
+          <Button title="Login" onPress={() => this.checkLogin()} />
+        </View>
       </View>
     );
   }
---
+++ app/component/Home/style.js
@@ -0,0 +1,26 @@
+import {StyleSheet} from 'react-native';
+export default StyleSheet.create({
+  heading: {
+    fontSize: 25,
+    textAlign: 'center',
+    marginTop: 50,
+    marginBottom: 10,
+    fontWeight: 'bold',
+  },
+  description: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginBottom: 30,
+    color: '#37474F',
+  },
+  input: {
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  viewButton: {
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+});
